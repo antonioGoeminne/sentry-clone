0896afb7491e21966249d306d338b6869c5416fe
antonioGoeminne <antonionicolas3003@gmail.com>
2023-10-28 19:25:23 -0300

    FEAT: agregando sidebar para desktop con animacion de framer motion y links a paginas

---
+++ src/ui/colors.ts
@@ -0,0 +1,5 @@
+export const primary = "#38173b";
+export const primaryLighter = "#ce99d9";
+export const primaryDarker = "#300c34";
+export const secondary = "#ca0c47";
+export const secondaryDarker = "#e81652";
